2875b75ac6b011184901440c541603f3e8e7b4d4
BrunoVian <bruno.vian@edu.unipar.br>
2023-04-14 20:32:45 -0300

    initial commit
    initial commit

---
+++ TrabalhoBimestralMobile/app/src/main/java/br/unipar/trabalhobimestralmobile/model/Cliente.java
@@ -0,0 +1,25 @@
+package br.unipar.trabalhobimestralmobile.model;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+}
